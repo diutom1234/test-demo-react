5df7b0aae6ec9cf713376df021cac166f21fb8da
unknown <ngodieu10102001@gmail.com>
2023-05-02 14:03:10 +0700

    add 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,11 +11,19 @@ class MyComponent extends React.Component {
         age: 18
     }
 
+    handleClick(event) {
+        console.log('>>> Click me my button');
+    }
+    handleMouseOver(event) {
+        console.log('My name is', this.state.name);
+    }
     //JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} - {this.state.address} - {this.state.age}
+                <button onClick={this.handleClick}>Hover me</button>
+                <button onMouseOver={this.handleMouseOver}>Click me</button>
             </div>
         );
     }

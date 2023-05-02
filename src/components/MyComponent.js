5730c343a6263aba4dce9e1d9774d240d58a1940
unknown <ngodieu10102001@gmail.com>
2023-05-02 16:13:04 +0700

    add 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,15 +3,18 @@
 
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
-
     //JSX
     render() {
+        const myInfor = ['a', 'b', 'c']
         return (
             <div>
                 <UserInfor />
-            </div>
+                <DisplayInfor name="Dieu" age={22} />
+                <DisplayInfor name="Dieu xinh" age={18} myInfor={myInfor} />
+            </div >
         );
     }
 }

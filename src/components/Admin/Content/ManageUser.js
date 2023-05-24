f3c216b628010c3084eef2c010dc87bb908ffeea
unknown <ngodieu10102001@gmail.com>
2023-05-24 18:30:33 +0700

    add #56b

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,13 +1,19 @@
-
+import ModalCreateUser from "./ModalCreateUser";
 
 const ManageUser = (props) => {
     return (
         <div className="manage-user-container">
             <div className="title">
-                ManageUser
+                Manage User
             </div>
             <div className="users-content">
-
+                <div>
+                    <button>Add new user</button>
+                </div>
+                <div>
+                    Table users
+                    <ModalCreateUser />
+                </div>
             </div>
         </div>
     )
